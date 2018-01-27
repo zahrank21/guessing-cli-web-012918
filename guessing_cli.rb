dc030a6740ce931a9e1a471fb02dac664279d5e3
zahrank21 <zahrank@gmail.com>
2018-01-27 21:51:15 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,14 +2,14 @@ def run_guessing_game
 	puts "/Guess a number between 1 and 6."
 	roll = 1+rand(6)
 	user_input = gets.chomp.to_s
-    if user_input == roll.to_s
+    if user_input == roll
     	return "You guessed the correct number!"
     elsif user_input != roll
     	return "The computer guessed #{roll}."
     elsif user_input == "exit"
       puts "Goodbye!"
     else
-      puts "Invalid input "
+      puts "Invalid input"
     end
 
 
